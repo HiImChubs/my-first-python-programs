f18bd0cd6bc37cc13820ecfcfedeaf74d1668d2a
HiImChubs <31480625+HiImChubs@users.noreply.github.com>
2017-08-30 11:05:42 -0400

    Add files via upload

---
+++ madlib-code.py
@@ -0,0 +1,22 @@
+print("Welcome to the madlib of the arrow enter the info below or perish") 
+print("Type a Name")
+name = input()
+print("Type Any Amount of Years")
+year = input()
+print("Type An Adjective")
+adj1 = input()
+print("Type 1st Verb")
+verb1 = input()
+print("Type 1st Noun")
+noun1 = input()
+print("Type 2nd Verb")
+verb2 = input()
+print("Type 3rd Verb")
+verb3 = input()
+print("Type 2nd Noun")
+noun2 = input()
+print( "my name is " + name + " for " + year + " years, i was stranded on a " + adj1 +
+       " island with only one goal " + verb1 + " Now, I will fufill my fathers dying wish to use the " + noun1 +
+       " of names he left and bring down those who are trying to " + verb2 + " my city I must become someone else. I am the " + noun2)
+
+
---
+++ my-first-program.py
@@ -0,0 +1,14 @@
+#This progam says hello and greets a person by name
+#
+#Connor Whiteside
+#August 25, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is a good to meet you, " + name + ".")
+print("Where were you born, " + name + "?")
+city = input()
+print("That's hella lit It'd be fire to slide in " + city + " one day")
+
+    
